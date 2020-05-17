d0e19987038a158fc6b8f737de20c137f4820493
VANISHKA DAS <dass10@myunitec.ac.nz>
2020-05-18 10:16:15 +1200

    used while loop

--- week5/class1.php
+++ week5/class1.php
@@ -7,4 +7,10 @@ echo "<h1>Hello world</h1>";
 echo "<p>Hello World</p>";
 
 echo "<p style='color: red'>Hello world</p>";
+//https://htcs5606s12020shweta.herokuapp.com/
 
+$i = 0;
+while ($i <100){ //set condition
+    echo "<p>Hello</p>"; //do something if condition is true
+    $i = $i + 1; //update counter
+}

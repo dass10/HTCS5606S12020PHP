5d03d89d8d052bc1ba73fc2b5b215445eb38aab3
VANISHKA DAS <dass10@myunitec.ac.nz>
2020-05-20 09:43:31 +1200

    created

--- week5/calculate.php
+++ week5/calculate.php
@@ -1,1 +1,11 @@
 <?php
+
+if (isset($_POST["width"])){ //isset check the post data
+    $w = $_POST["width"];
+    $h = $_POST["height"];
+    $area = $w * $h;
+    echo  "Area: ".$area;
+}else{
+    echo  "go back to form page";
+
+}

87824e19231cf9e923bd0a9f52df65d928091da1
VANISHKA DAS <dass10@myunitec.ac.nz>
2020-05-25 10:07:03 +1200

    show records

--- week6/showrecords.php
+++ week6/showrecords.php
@@ -10,3 +10,16 @@ if ($connection->connect_error){
 }else{
     echo "Connection Created";
 }
+
+$sql = "select * from user"; //create query
+$result = $connection->query($sql);//run the query on this connection
+
+if ($result->num_rows> 0){
+    while($row = $result-> fetch_assoc()){ //check if there is record in the result
+        echo $row['id']." ".$row['username']." ".$row['password']. " ".$row['name']. "<br>"; // in each row,we have comumns.
+
+    }
+}else{
+    echo "no result in the table";
+}
+$connection->close();

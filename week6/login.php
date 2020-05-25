feac20185458f575539736900cdc89a0effb6615
VANISHKA DAS <dass10@myunitec.ac.nz>
2020-05-26 09:18:05 +1200

    updt

---
+++ week6/login.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Author: Shweta Das
+ * Date: 26 May 2020
+ * Version: 1.0
+ * Purpose: for login
+ */
+
+if (isset($_POST["username"])) {
+
+    $username = $_POST["username"];
+    $password = $_POST["pwd"];
+    $server = "m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $user = "sp8kxui59yxqk3uy";
+    $pwd = "vfb198mevrvl8nl7";
+    $database = "b1m6lvq858bqg11k";
+
+    $connection = new mysqli($server, $user, $pwd, $database);
+    if ($connection->connect_error) {
+        echo $connection->connect_error;
+    } else {
+        echo "Connection Created";
+    }
+}
+

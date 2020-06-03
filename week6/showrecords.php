d40d8e32ef088b7fde9c77d6bc8cb53ec3091979
VANISHKA DAS <dass10@myunitec.ac.nz>
2020-06-04 09:58:21 +1200

    updt

--- week6/showrecords.php
+++ week6/showrecords.php
@@ -8,7 +8,7 @@ while ($i <sizeof($records)){
     // print_r($records[$i]; //arrayname[index] --> value
     $record = $records[$i]; //inside each record,we have place 0 for id, place 1 for username,place 2 for passowrd,
     ?>
-<p><?php  $record->id; ?>
+    <p><?php  $record->id; ?>
     <?php echo $record->getUsername(); ?>
     <?php echo $record->getpassword(); ?>
     <?php echo $record->name; ?>

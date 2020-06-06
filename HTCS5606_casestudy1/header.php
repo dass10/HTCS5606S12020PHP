26ed3e15de948e52f7d46acd73638002f0b9c8a6
VANISHKA DAS <dass10@myunitec.ac.nz>
2020-06-06 22:02:38 +1200

    updt

--- HTCS5606_casestudy1/header.php
+++ HTCS5606_casestudy1/header.php
@@ -18,9 +18,9 @@
     <div class="level1">
         <a href="index.php">Home</a>
     </div>
-    <div class="level1">
+    <div class="level2">
         <a href="shopbypet.php" class="topItem">Shop by Pet</a>
-        <div class="level1">
+        <div class="level2">
             <p><a href="#">Dog Food </a></p>
             <p><a href="#">Cat Food</a></p>
             <p><a href="#">Bird Food</a></p>

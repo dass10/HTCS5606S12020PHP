8d60f47e835b809e66ab2718b44936fe6350dae8
VANISHKA DAS <dass10@myunitec.ac.nz>
2020-06-06 22:19:05 +1200

    updt

--- HTCS5606_casestudy1/JS/script.js
+++ HTCS5606_casestudy1/JS/script.js
@@ -28,33 +28,11 @@ $(document).ready(function () {
         $(this).next().css("display", "block");
     });
     $(".level1").mouseleave(function () {
-
-        $(this).next().css("display", "block");
-    });
-    $(".level2").mouseleave(function () {
-
+        // I don't care this divider has children divider (class: level2) or not,
+        // if it has level2 divider, when my mouse left, I hide this divider.
         $(this).children("div").hide();
     });
-    $(".level3").mouseleave(function () {
-
-        $(this).next().css("display", "block");
-    });
-    $(".level4").mouseleave(function () {
-
-        $(this).next().css("display", "block");
-    });
-    $(".level5").mouseleave(function () {
-
-        $(this).next().css("display", "block");
-    });
-    $(".level6").mouseleave(function () {
-
-        $(this).next().css("display", "block");
-    });
-
 });
-
-
 
 //add to cart
 

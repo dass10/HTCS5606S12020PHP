9a0036a1620600ffccc56a3092e66a5cd17fdb68
VANISHKA DAS <dass10@myunitec.ac.nz>
2020-06-07 13:21:11 +1200

    updt

--- HTCS5606_casestudy1/index.php
+++ HTCS5606_casestudy1/index.php
@@ -21,6 +21,11 @@
     <?php
     include_once "header.php";
     ?>
+    <?php
+    include_once "footer.php";
+    ?>
+
+
     <div id="borderimg">
         <img src="image/kitty.jpeg" id="kitty"/>
         <img src="image/guinea pig.jpeg" id="img2"/>
@@ -51,44 +56,6 @@
 
 
 </div>
-<div class="footer">
-    <div class="inner-footer">
-        <div class="footer-items">
-            <h1>Help</h1>
-            <p>Shipping</p>
-            <p>Privacy Policy</p>
-            <p>Terms & Conditions</p>
-            <p>Return & Exchange</p>
-        </div>
-        <div class="footer-items">
-            <h1>Company Info</h1>
-            <p>About us</p>
-            <p>FAQ</p>
-            <p>Contact us</p>
-        </div>
 
-        <div class="footer-items">
-            <h1>Connect With us</h1>
-
-
-            <a href="https://www.instagram.com/"><img src="image/insta.jpg" height="20" width="20"/></a>
-
-            <a href="https://twitter.com/"><img src="image/twitter.jpg" height="20" width="20"/></a>
-
-            <a href="https://www.facebook.com/"><img src="image/fb.jpg" height="20" width="20"/><br><br></a>
-
-
-            <img src="https://dass10.github.io/HTCS5606_casestudy1/image/call us.jpg"  height="50" width="50" /> +64220440190
-
-
-        </div>
-
-        <div class="footer-items">
-            <h1>Join our NewsLetter</h1>
-            <p>Be the first to Hear about our New Arrivals and Special Deals</p>
-            <img src="https://dass10.github.io/HTCS5606_casestudy1/image/sign up.png" height="270" width="270"/>
-        </div>
-    </div>
-</div>
 </body>
 </html>

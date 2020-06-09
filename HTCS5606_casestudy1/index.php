d84b92172bedc5b9e0276135b6f13b72bd9a79c3
VANISHKA DAS <dass10@myunitec.ac.nz>
2020-06-10 08:58:48 +1200

    updt

--- HTCS5606_casestudy1/index.php
+++ HTCS5606_casestudy1/index.php
@@ -6,6 +6,10 @@
     <title>Shweta Assessment 1 Home page</title>
     <link rel="stylesheet" href="CSS/style.css">
     <script src="JS/script.js"></script>
+    <script
+            src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous"></script>
 
 </head>
 <body id="page1">
--- HTCS5606_casestudy1/register.php
+++ HTCS5606_casestudy1/register.php
@@ -5,6 +5,10 @@
     <title>log in/Register</title>
     <script src="JS/script.js"></script>
     <link rel="stylesheet" href="CSS/style.css">
+    <script
+            src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous"></script>
 
 </head>
 <body>

0933a78ae6be2f600a684ed1749ca96286e8aef1
VANISHKA DAS <dass10@myunitec.ac.nz>
2020-06-10 21:25:56 +1200

    updt

--- HTCS5606_casestudy1/index.php
+++ HTCS5606_casestudy1/index.php
@@ -1,19 +1,23 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Shweta Assessment 1 Home page</title>
     <link rel="stylesheet" href="CSS/style.css">
-    <script src="JS/script.js"></script>
-    <script
-            src="https://code.jquery.com/jquery-3.5.1.min.js"
-            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-            crossorigin="anonymous"></script>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="JS/script.js"></script>
 </head>
 <body id="page1">
+<div id="header">
+    <div class="topnav">
+        <img src="image/checkout.png" height="50" width="50"/>
+        <input type="text" placeholder="Search..">
+    </div>
 
+    <h3><img src="image/logo.png" height="50" width="50"/><br>
+        The Pet Meal
+    </h3>
     <?php
     include_once "header.php";
     ?>
@@ -27,26 +31,26 @@
         <img src="image/dog.jpeg" id="img5"/>
     </div>
 
-<div id="main">
+    <div id="main">
 
-<div id ="border box">
-    New Zealand's No.1 Pet Food online Service With 3 Years of Experience.The Pet Meal across NZ has over 3 years of experience in providing  pet food.
-    We are the No.1 online store for frozen,Wet and Dry dog and cat food,also offering a wide range of great quality pet food for Fish,Chicken,reptiles and small Animals.
-    from market leader manufacturers.<br><br>
+        <div id ="border box">
+            New Zealand's No.1 Pet Food online Service With 3 Years of Experience.The Pet Meal across NZ has over 3 years of experience in providing  pet food.
+            We are the No.1 online store for frozen,Wet and Dry dog and cat food,also offering a wide range of great quality pet food for Fish,Chicken,reptiles and small Animals.
+            from market leader manufacturers.<br><br>
 
-    There's no need to worry about running out of food..not anymore!
-    Save time by using a fast and efficient pet food home delivery service near you.<br><br>
+            There's no need to worry about running out of food..not anymore!
+            Save time by using a fast and efficient pet food home delivery service near you.<br><br>
 
-    We stock the best quality pet food online, including Natures Menu  Dog Food, Natural cat food & many others.
-    Mobile Pet Foods have been offering an exceptional Delivery Service for 3 years now. Shop with confidence at competitive prices.
-    Our delivery service includes:<br><br>
+            We stock the best quality pet food online, including Natures Menu  Dog Food, Natural cat food & many others.
+            Mobile Pet Foods have been offering an exceptional Delivery Service for 3 years now. Shop with confidence at competitive prices.
+            Our delivery service includes:<br><br>
 
-    ✔ cat food delivery ✔ dog food delivery ✔ fish, bird & rabbit food delivery
-    <br><br>
-    Click & Collect (now available at our Auckland and Wellington store.)
+            ✔ cat food delivery ✔ dog food delivery ✔ fish, bird & rabbit food delivery
+            <br><br>
+            Click & Collect (now available at our Auckland and Wellington store.)
 
-</div>
-</div>
+        </div>
+    </div>
     <?php
     include_once "footer.php";
     ?>
@@ -54,3 +58,4 @@
 
 </body>
 </html>
+

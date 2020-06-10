c388c4fabe87e1e62b69c91a7c981058214fde03
VANISHKA DAS <dass10@myunitec.ac.nz>
2020-06-11 08:52:39 +1200

    updt

--- HTCS5606_casestudy1/header.php
+++ HTCS5606_casestudy1/header.php
@@ -3,7 +3,7 @@
 
 <div id="header">
     <div class="topnav">
-        <a href="checkout.php"</a><img src="image/checkout.png" height="50" width="50"/>
+       <img src="image/checkout.png" height="50" width="50"/> <a href="checkout.php"</a>
         <input type="text" placeholder="Search..">
     </div>
 

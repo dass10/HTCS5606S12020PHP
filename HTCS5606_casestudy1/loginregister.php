85a2ef3298380d578cfa0c36dd11090aeafdad9b
shweta <dass10@myunitec.ac.nz>
2020-08-13 22:53:11 +1200

    update

--- HTCS5606_casestudy1/loginregister.php
+++ HTCS5606_casestudy1/loginregister.php
@@ -12,15 +12,7 @@
 
 
 <div id="header">
-    <div class="topnav">
 
-        <input type="text" placeholder="Search..">
-    </div>
-
-
-    <h3><img src="image/logo.png" height="50" width="50"/><br>
-        The Pet Meal
-    </h3>
 
     <?php
     include_once "header.php";
